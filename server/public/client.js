9983b0304d7611b6aa9f9f5c9f8f0ab09daf331e
Toivo Hannigan <THannigan046@gmail.com>
2021-12-18 19:56:28 -0600

    completed weekend challenge 2

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,64 @@
 $(document).ready(onReady)
 
+let mathButton = ''
 function onReady() {
-    console.log('ready');
-    
+    console.log('JQ'); 
+    getHistory();
+    $('.mathButton').on('click', function () {
+        mathButton = $(this).html();
+    })
+    $('#equalsButton').on('click', evaluate)
+    $('#clearButton').on('click', clear)
+};
+
+function getHistory() {
+    $.ajax({
+        method: 'GET',
+        url: '/history'
+    }).then(
+        response => {
+            $('historyList').empty()
+            response.forEach(element => {
+                let historyItem = $(`
+                <li>${element.numerator} ${element.mathButton} ${element.denominator} = ${element.result}</li>
+                `);
+                $('#historyList').append(historyItem);
+                historyItem.data('id', element.id);
+            })
+        }
+
+    )
+}
+
+function evaluate() {
+    let numerator = $('#numeratorInput').val()
+    let denominator = $('#denominatorInput').val()
+    let equalsObj = {
+         numerator: numerator,
+         denominator: denominator,
+         mathButton: mathButton
+        }
+    if (numerator === '' || denominator === '' || mathButton === '') {
+        return;
+    }
+    mathButton = ''
+    $.ajax({
+        method: 'POST',
+        url: '/evaluate',
+        data: equalsObj
+    }).then(
+            response => {
+            let result = response.result
+            console.log('POST response', response);
+            $('#result').html(`<h3>${result}</h3>`);
+            getHistory()
+
+        });
+}
+
+
+function clear() {
+    $('#numeratorInput').val('')
+    $('#denominatorInput').val('')
+    $('#result').empty();
 }
--- server/server.js
+++ server/server.js
@@ -2,12 +2,52 @@ const express = require('express')
 
 const app = express();
 
-app.use(express.static('server/public'));
-
 const bodyParser = require('body-parser')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(express.static('server/public'));
+const historyArray = [];
+let id = 0 
+
+app.get('/history', (req, res) => {
+    res.send(historyArray);
+})
+//calculation goes here
+app.post('/evaluate', (req, res) => {
+    let numerator = req.body.numerator
+    let denominator = req.body.denominator
+    let mathButton = req.body.mathButton
+    let result = 0
+    switch (mathButton) {
+        case '+':
+        result = Number(numerator) + Number(denominator);
+        break;
+        case '-':
+            result = Number(numerator) - Number(denominator);
+            break;
+        case '*':
+            result = Number(numerator) * Number(denominator);
+            break;
+        case '/':
+            result = Number(numerator) / Number(denominator);
+            break;
+    };
+    let equalsObj = {
+       numerator: numerator,
+       denominator: denominator,
+       mathButton: mathButton, 
+       result: result, 
+       id: id
+    }
+    id += 1;
+    historyArray.push(equalsObj);
+    res.send(equalsObj);
+})
+
+
+//port init 
 const port = 5000;
 app.listen(port, () => {
     console.log('I/m listening');
